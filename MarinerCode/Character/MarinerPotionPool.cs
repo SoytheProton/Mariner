c63e444a573920461cb8b1f7f40bc69e6f3977c6
SoytheProton <alttimelinemod@gmail.com>
2026-07-21 18:37:08 -0700

    mariner real or something idk

---
+++ MarinerCode/Character/MarinerPotionPool.cs
@@ -0,0 +1,14 @@
+﻿using BaseLib.Abstracts;
+using Mariner.MarinerCode.Extensions;
+using Godot;
+
+namespace Mariner.MarinerCode.Character;
+
+public class MarinerPotionPool : CustomPotionPoolModel
+{
+    public override Color LabOutlineColor => Mariner.Color;
+
+
+    public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
+    public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+}
---
+++ MarinerCode/Potions/MarinerPotion.cs
@@ -0,0 +1,8 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using Mariner.MarinerCode.Character;
+
+namespace Mariner.MarinerCode.Potions;
+
+[Pool(typeof(MarinerPotionPool))]
+public abstract class MarinerPotion : CustomPotionModel;
