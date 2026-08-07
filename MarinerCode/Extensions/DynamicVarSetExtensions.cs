f5aa6f4f0a12e91868683c7d6ac56a173d66b4b3
SoytheProton <alttimelinemod@gmail.com>
2026-08-07 12:44:48 -0700

    extension

---
+++ MarinerCode/Extensions/DynamicVarSetExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Mariner.MarinerCode.Cards.Variables;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Mariner.MarinerCode.Extensions;
+
+public static class DynamicVarSetExtensions
+{
+    public static SubmergeVar Submerge(this DynamicVarSet vard)
+    {
+        return (SubmergeVar)vard._vars[nameof(Submerge)];
+    }
+    
+    public static DredgeVar Dredge(this DynamicVarSet vard)
+    {
+        return (DredgeVar)vard._vars[nameof(Dredge)];
+    }
+}
