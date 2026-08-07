594a2c2b311c13e513f937348d29ce57b97c9cf1
Aurora <officialbubbletanks@gmail.com>
2026-08-07 19:22:17 -0400

    stuff

---
+++ MarinerCode/Cards/Common/AnchorShot.cs
@@ -0,0 +1,29 @@
+﻿using Mariner.MarinerCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Mariner.MarinerCode.Cards.Common;
+
+public class AnchorShot() : MarinerCard(0,
+    CardType.Attack, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(5M, ValueProp.Move)];
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [MarinerCardKeywords.Trawl];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this, play).Targeting(play.Target).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3M);
+    }
+}
---
+++ MarinerCode/Cards/Common/BarnacleBloom.cs
@@ -0,0 +1,37 @@
+﻿using Mariner.MarinerCode.Cards;
+using Mariner.MarinerCode.Cards.Variables;
+using Mariner.MarinerCode.Commands;
+using Mariner.MarinerCode.Extensions;
+using Mariner.MarinerCode.Monsters;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Mariner.MarinerCode.Cards.Common;
+
+public class BarnacleBloom() : MarinerCard(1,
+    CardType.Attack, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(9M, ValueProp.Move), new RepeatVar(2)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        AttackCommand attackCommand = await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this, play).Targeting(play.Target).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+        if (attackCommand.Results.SelectMany(r => r).Any((Func<DamageResult, bool>) (r => r.WasTargetKilled)))
+        {
+            for(int i = 0; i < DynamicVars.Repeat._baseValue; i++) BarnacleCmd.Spawn(choiceContext, Owner, this);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(4M);
+    }
+}
