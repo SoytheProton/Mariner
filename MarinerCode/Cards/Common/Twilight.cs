dd552733b9e0c9965d5b5b70f35580e805b958b0
Aurora <officialbubbletanks@gmail.com>
2026-08-07 21:59:07 -0400

    skills

---
+++ MarinerCode/Cards/Common/Twilight.cs
@@ -0,0 +1,43 @@
+﻿using Mariner.MarinerCode.Cards.Variables;
+using Mariner.MarinerCode.Commands;
+using Mariner.MarinerCode.Extensions;
+using Mariner.MarinerCode.Interfaces;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Mariner.MarinerCode.Cards.Common;
+
+public class Twilight() : MarinerCard(1,
+    CardType.Skill, CardRarity.Common,
+    TargetType.Self), ISubmergeHook
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<WeakPower>(2)];
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [MarinerCardKeywords.Trawl];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PowerCmd.Apply<WeakPower>(choiceContext, play.Target, DynamicVars.Weak.BaseValue, Owner.Creature, this);
+    }
+
+    public async Task AfterCardSubmerged(
+        PlayerChoiceContext choiceContext, CardModel card)
+    {
+        if (card != this)
+            return;
+        SetToFreeThisTurn();
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Weak.UpgradeValueBy(1M);
+    }
+}
