a440e55e92fbefe8b0606b1bfd9a84c114ec4024
SoytheProton <alttimelinemod@gmail.com>
2026-08-16 21:58:27 -0700

    changes i never committed until now

--- MarinerCode/Cards/Basic/WashedAway.cs
+++ MarinerCode/Cards/Basic/WashedAway.cs
@@ -6,17 +6,18 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Mariner.MarinerCode.Cards.Basic;
 
 [Pool(typeof(MarinerCardPool))]
 public sealed class WashedAway() : MarinerCard(2, CardType.Attack,
-    CardRarity.Basic, TargetType.AnyEnemy), ISunkenCard
+    CardRarity.Basic, TargetType.AnyEnemy), IAbyssalCard
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(16M, ValueProp.Move), new DamageVar("Damage2", 6M, ValueProp.Move)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(12M, ValueProp.Move), new PowerVar<WeakPower>(1)];
     
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(MarinerStaticHovertip.Sunken)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(MarinerStaticHovertip.Abyssal), HoverTipFactory.FromPower<WeakPower>()];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
@@ -25,14 +26,13 @@ public sealed class WashedAway() : MarinerCard(2, CardType.Attack,
         await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this, play).Targeting(play.Target).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
     }
 
-    public async Task OnSunken(PlayerChoiceContext choiceContext)
+    public async Task BeforeShuffled(PlayerChoiceContext choiceContext)
     {
-        await CreatureCmd.Damage(choiceContext, CombatState.HittableEnemies, DynamicVars["Damage2"]._baseValue, ValueProp.Move, Owner.Creature);
+        await PowerCmd.Apply<WeakPower>(choiceContext, CombatState?.HittableEnemies, DynamicVars.Weak.BaseValue, Owner.Creature, this);
     }
-    
+
     protected override void OnUpgrade()
     {
         DynamicVars.Damage.UpgradeValueBy(4M);
-        DynamicVars["Damage2"].UpgradeValueBy(2M);
     }
 }
---
+++ MarinerCode/Patches/CreaturePatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Mariner.MarinerCode.Monsters;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+
+namespace Mariner.MarinerCode.Patches;
+
+public class CreaturePatch
+{
+    [HarmonyPatch(typeof(Creature), nameof(Creature.ScaleMonsterHpForMultiplayer))]
+    public class FilterForCombatPatch
+    {
+        public static bool Prefix(Creature __instance)
+        {
+            return __instance.Monster is not Barnacle;
+        }
+    }
+}
